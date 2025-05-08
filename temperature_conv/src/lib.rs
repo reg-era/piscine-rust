99974ce5ae07ceeba068e5d6747d22d4c39a9bd5
reg-era <ilyass.med.fkh@gmail.com>
2025-05-08 15:18:52 +0100

    temperature 3

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,11 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    // °C = (°F - 32) × 5/9
-    return (f - 32.) * 5. / 9.;
+    return (f - 32.) / 1.8;
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    // °F = (9/5 × °C) + 32
-    return (9. / 5. * c ) + 32.0;
+    return (9. / 5. * c) + 32.;
 }
 
 #[cfg(test)]
@@ -16,5 +14,10 @@ mod tests {
     fn it_works() {
         assert_eq!(fahrenheit_to_celsius(-459.67), -273.15);
         assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
+
+        assert_eq!(fahrenheit_to_celsius(20.), -6.666666666666666);
+        assert_eq!(fahrenheit_to_celsius(83.), 28.333333333333332);
+
+        assert_eq!(celsius_to_fahrenheit(27.), 80.6);
     }
 }
