6a2d5f98463dd6619037a3a93b09cbaeb56bbfa8
reg-era <ilyass.med.fkh@gmail.com>
2025-05-08 16:19:53 +0100

    groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    return &slice[index];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
