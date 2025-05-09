bebead1dda33d0c78a629b1038f00c03f7ebe06d
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-09 17:42:19 +0100

    name init

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut res = vec![String::new(); names.len()];
+    let mut i = 0;
+    for word in names {
+        let mut new_word = String::new();
+        for sp in word.split(" ") {
+            new_word.push(sp.chars().nth(0).expect("wiiw"));
+            new_word.push_str(". ");
+        }
+        let (splited, _) = new_word.split_at(new_word.len() - 1);
+        res[i] = String::from(splited);
+        i += 1;
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
+        println!("{:?}", initials(names));
+    }
+}
