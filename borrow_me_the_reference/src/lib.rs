d33387fbe50c5f24e7c33e3b51b3c1ab8368694f
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-09 19:57:04 +0100

    comit

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,28 +1,34 @@
 pub fn delete_and_backspace(s: &mut String) {
-    let mut saved = "";
+    let mut copy = String::new();
     let mut index = 0;
-    let mut move_up = false;
     for c in s.chars() {
-        if c == '+' {
-            move_up = true;
-            index += 1;
-        }
         if c == '-' {
-            move_up = true;
+            copy.pop();
+        } else if c == '+' {
+            index += 1;
+        } else if index > 0 {
             index -= 1;
-        }
-        if c != '-' && c != '+'  {
-            if move_up && index > 0{
-                move_up = false;
-                *s[index] = 'd';
-            }else{
-                s.push(c);
-            }
+        } else {
+            copy.push(c);
         }
     }
+    *s = copy;
 }
 
-// pub fn do_operations(v: &mut [String]) {}
+// pub fn do_operations(v: &mut [String]) {
+    // 
+    // for c in s.chars() {
+        // if c == '-' {
+            // copy.pop();
+        // } else if c == '+' {
+            // index += 1;
+        // } else if index > 0 {
+            // index -= 1;
+        // } else {
+            // copy.push(c);
+        // }
+    // }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -31,16 +37,16 @@ mod tests {
     #[test]
     fn it_works() {
         let mut a = "bpp--o+er+++sskroi-++lcw".to_owned();
-        let mut b = [
-            "2+2".to_owned(),
-            "3+2".to_owned(),
-            "10-3".to_owned(),
-            "5+5".to_owned(),
-        ];
+        // let mut b = [
+            // "2+2".to_owned(),
+            // "3+2".to_owned(),
+            // "10-3".to_owned(),
+            // "5+5".to_owned(),
+        // ];
 
         delete_and_backspace(&mut a);
-        do_operations(&mut b);
+        // do_operations(&mut b);
 
-        println!("{:?}", (a, b));
+        println!("{:?}", (a));
     }
 }
