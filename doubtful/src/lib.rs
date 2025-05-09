945fc09e750c8f551dbfea69925fb0c083172191
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-09 16:35:37 +0100

    doobleful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn doubtful(s: &mut String) {
+    s.push('?');
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut s = "Hello".to_owned();
+
+        println!("Before changing the string: {}", s);
+        doubtful(&mut s);
+        println!("After changing the string: {}", s);
+    }
+}
