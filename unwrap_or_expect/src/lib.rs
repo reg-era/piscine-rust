c2e44dd810cafc80de9b5e1a056b34d2a2a1ece2
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-15 15:47:48 +0100

    unwrap 4

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -9,7 +9,7 @@ pub enum Security {
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match server {
         Ok(m) => match security_level {
-            Security::UnexpectedUrl => String::from(m),
+            Security::UnexpectedUrl => panic!("{}", m),
             _ => String::from(m),
         },
         Err(m) => match security_level {
@@ -21,28 +21,7 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
             }
             Security::Warning => String::from("WARNING: check the server"),
             Security::NotFound => String::from(format!("Not found: {}", m)),
-            Security::UnexpectedUrl => panic!("{}", m),
+            Security::UnexpectedUrl => String::from(m),
         },
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        println!("{}", fetch_data(Ok("server1.com"), Security::Warning));
-        println!("{}", fetch_data(Err("server.com"), Security::Warning));
-        println!("{}", fetch_data(Err("server2.com"), Security::NotFound));
-
-        // Panics with no custom message
-        // fetch_data(Err("ERROR CRITICAL"), Security::Unknown);
-
-        // Panics with the message "ERROR: program stops"
-        // fetch_data(Err("server.com"), Security::Message);
-
-        // Panics with the message "malicious_server.com"
-        // fetch_data(Ok("malicious_server.com"), Security::UnexpectedUrl);
     }
 }
