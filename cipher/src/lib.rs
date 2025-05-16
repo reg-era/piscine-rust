bc6769745602b3d95f7321e81ce536dadf2b67e2
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-16 16:15:43 +0100

    cipher 2

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, PartialEq)]
 pub struct CipherError {
-    expected: String,
+    pub expected: String,
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
