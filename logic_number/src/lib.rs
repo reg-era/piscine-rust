2aa18f188dda5a554f4dee15108f86c6fd0a5f3c
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-05-18 12:02:46 +0100

    logic num

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn number_logic(num: u32) -> bool {
+    fn get_len(mut nb: u32) -> u32 {
+        let mut count = 0;
+        while nb > 0 {
+            nb = (nb as i32 / 10) as u32;
+            count += 1;
+        }
+        count
+    }
+
+    let len = get_len(num);
+    let mut copy = num as i32;
+
+    let mut splited: Vec<i32> = Vec::new();
+    while copy > 0 {
+        let last = copy % 10;
+        splited.push(last);
+        copy = copy /10;
+    }
+
+    let mut total = 0;
+    splited.into_iter().map(|n| n.pow(len)).for_each(|n| total += n);
+
+    total as u32 == num
 }
 
 #[cfg(test)]
@@ -8,7 +30,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let array = [9, 10, 153, 154];
+        for pat in &array {
+            if number_logic(*pat) == true {
+                println!(
+                    "this number returns {} because the number {} obey the rules of the sequence",
+                    number_logic(*pat),
+                    pat
+                )
+            }
+            if number_logic(*pat) == false {
+                println!("this number returns {} because the number {} does not obey the rules of the sequence", number_logic(*pat),pat )
+            }
+        }
     }
 }
