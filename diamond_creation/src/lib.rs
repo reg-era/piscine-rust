99fe1c4993341727ada2da4a038417741604f033
REGERA <ilyass.med.fkh@gmail.com>
2025-05-19 15:31:51 +0100

    diamand creation

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -1,5 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn get_diamond(c: char) -> Vec<String> {
+    let mut res = Vec::new();
+
+    let n = c as u8 - b'A';
+    for i in 0..=n {
+        let letter = (b'A' + i) as char;
+        let born_spc = (n - i) as usize;
+        let midlle_spc = if i == 0 { 0 } else { (2 * i - 1) as usize };
+
+        let line = if i == 0 {
+            format!("{}{}{}", " ".repeat(born_spc), letter, " ".repeat(born_spc))
+        } else {
+            format!(
+                "{}{}{}{}{}",
+                " ".repeat(born_spc),
+                letter,
+                " ".repeat(midlle_spc),
+                letter,
+                " ".repeat(born_spc)
+            )
+        };
+
+        res.push(line);
+    }
+
+    for i in (0..n).rev() {
+        res.push(res[i as usize].clone());
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -8,7 +36,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        println!("{:?}", get_diamond('A'));
+        println!("{:?}", get_diamond('C'));
+        for line in get_diamond('C') {
+            println!("{}", line);
+        }
     }
 }
